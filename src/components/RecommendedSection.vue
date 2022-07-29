462b2ae5f613a1cb837da78d81ad5bc860e15549
Rafael Vargas <rvargas@mcfadyen.com>
2022-07-29 14:05:06 -0300

    Creation of the recommended section

---
+++ src/components/RecommendedSection.vue
@@ -0,0 +1,42 @@
+<template>
+  <section id="recommended">
+    <h1>Recommended Titles</h1>
+    <div class="wrapper">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from './Card.vue';
+
+export default {
+  name: 'RecommendedSection',
+  components: {
+    Card,
+  },
+};
+
+</script>
+
+<style lang="scss" scoped>
+#recommended {
+  height: fit-content;
+  padding: 20px;
+  width: 100%;
+
+  h1 {
+    font-size: 1.5em;
+  }
+
+  .wrapper {
+    display: flex;
+    flex-wrap: wrap;
+  }
+}
+</style>
