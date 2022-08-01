985847017b1daacc19d22167d2d0ab6462455fe6
Rafael <rvargas@mcfadyen.com>
2022-08-01 09:24:31 -0300

    Creates the recommended section of the application
    This might become a screen later

--- src/components/RecommendedSection.vue
+++ src/components/RecommendedSection.vue
@@ -1,24 +1,65 @@
 <template>
-  <section id="recommended">
-    <h1>Recommended Titles</h1>
-    <div class="wrapper">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
+  <section id="recommended" v-if="!searching">
+    <MoviesFragment category="Movies" :data="movies" />
+    <MoviesFragment category="TV Shows" :data="tvShows" />
+  </section>
+  <section id="search" v-else>
+    <MoviesFragment category="Search" :data="search" :totalResults="totalResults" :query="query" />
   </section>
 </template>
 
 <script>
-import Card from './Card.vue';
+import MoviesFragment from './MoviesFragment.vue';
 
 export default {
   name: 'RecommendedSection',
   components: {
-    Card,
+    MoviesFragment,
+  },
+  props: {
+    category: {
+      type: String,
+      require: true,
+    },
+  },
+  data() {
+    return {
+      movies: [],
+      tvShows: [],
+      searching: false,
+      search: [],
+      query: '',
+      totalResults: 0,
+    };
+  },
+  async created() {
+    this.$store.commit('setMovies');
+  },
+  computed: {
+    getShowsAndMovies() {
+      return this.$store.getters.getAll;
+    },
+    getSearch() {
+      return this.$store.getters.getSearch;
+    },
+    getSearchState() {
+      return this.$store.getters.getSearching;
+    },
+  },
+  watch: {
+    getShowsAndMovies(data) {
+      this.movies = data.movies;
+      this.tvShows = data.shows;
+    },
+    getSearch(data) {
+      this.searching = this.$store.getters.getSearching;
+      this.search = data.result;
+      this.totalResults = data.total_results;
+      this.query = data.query;
+    },
+    getSearchState(data) {
+      this.searching = data;
+    },
   },
 };
 
@@ -27,16 +68,7 @@ export default {
 <style lang="scss" scoped>
 #recommended {
   height: fit-content;
-  padding: 20px;
   width: 100%;
-
-  h1 {
-    font-size: 1.5em;
-  }
-
-  .wrapper {
-    display: flex;
-    flex-wrap: wrap;
-  }
+  padding-top: 30px;
 }
 </style>
