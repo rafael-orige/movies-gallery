1ba3c72e95de2032531db512ed9a7d0212ee6627
Rafael <rvargas@mcfadyen.com>
2022-08-01 09:19:19 -0300

    Header search functionality

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,42 +1,91 @@
 <template>
   <header>
-    <img src="../assets/logo.png" alt="logo" />
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>Movies</li>
-        <li>TV Shows</li>
-      </ul>
-    </nav>
+    <div class="header-left">
+      <img src="../assets/logo.png" alt="logo" />
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Movies</li>
+          <li>TV Shows</li>
+        </ul>
+      </nav>
+    </div>
+
+    <div class="header-right">
+      <label for="search">
+        <input
+          autocomplete="off"
+          v-model="input"
+          @input="debounce"
+          id="search"
+          type="search"
+          placeholder="Search..."
+        />
+        <button @click="search">Click to search</button>
+      </label>
+    </div>
   </header>
 </template>
 
 <script>
+import * as _ from 'lodash';
+
 export default {
   name: 'HeaderComponent',
+  data() {
+    return {
+      input: '',
+      query: '',
+    };
+  },
+  methods: {
+    search() {
+      if (!this.input) return;
+      this.$store.commit('setSearch', this.input);
+    },
+    debounce: _.debounce(function () {
+      this.query = this.input;
+    }, 1250),
+  },
+  computed: {
+    getQuery() {
+      return this.query;
+    },
+  },
+  watch: {
+    getQuery(query) {
+      this.$store.commit('setSearch', query);
+    },
+  },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
   header {
     align-items: center;
     background-color: #161d2f;
     border-radius: 5px;
     display: flex;
-    justify-content: left;
+    justify-content: space-between;
     padding: 15px;
     width: 100%;
 
-    nav {
-      margin-left: 40px;
+    .header-left {
+      align-items: center;
+      display: flex;
+
+      nav {
+          margin-left: 40px;
+        }
+
+      img {
+        cursor: pointer;
+        height: 40px;
+        width: auto;
+      }
     }
 
-    img {
-      height: 40px;
-      width: auto;
-      cursor: pointer;
-    }
   }
 
   ul {
@@ -46,9 +95,41 @@ export default {
     padding: 0;
 
     li {
-      padding: 5px 5px;
-      margin: 0 5px;
       cursor: pointer;
+      margin: 0 5px;
+      padding: 5px 5px;
+    }
+  }
+
+  label {
+    background-color: transparent;
+    border: 2px solid transparent;
+    border-bottom: 2px solid #273455;
+    color: white;
+
+    &:focus,
+    .active {
+      outline: none;
+      border-radius: 5px;
+      border: 2px solid #273455;
+    }
+
+    #search {
+      background-color: transparent;
+      border: none;
+      color: white;
+      padding: 5px 10px;
+
+      &:focus {
+        outline: none;
+      }
+    }
+
+    button {
+      border: none;
+      outline: none;
+      background: transparent;
+      color: white;
     }
   }
 </style>
